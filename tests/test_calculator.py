c16e129ff84b8c89670559f6f07dd13e00a1344e
madart954 <madart954@gmail.com>
2026-03-08 19:34:17 +0500

    добавил пару тестов исправил ошибку связанную с 0

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,16 +1,28 @@
 from calculator02 import is_number
 import calculator02 as cl
+import pytest
 
+@pytest.mark.parametrize("expression, expected", [
+    ("2+3", "5.0"),
+    ("2*3", "6.0"),
+    ("10/2", "5.0"),
+    ("-13.3+(-31.2)*0.1",str(-13.3 + (-31.2) * 0.1))
+])
+def test_calculator(expression, expected):
+    result = cl.Calculator(expression)
+    assert result.answer == expected
+
+
+@pytest.mark.parametrize("expression, expected",[
+    ("5",True),
+    ("0",True),
+    ("abc",False)
+])
 #тест статик функций
-def test_is_number_number():
-    assert is_number("5")
+def test_is_number_number(expression,expected):
+    assert is_number(expression) == expected
 
-def test_is_number_zero():
-    assert is_number("0")
-
-def test_is_number_abc():
-    assert is_number("abc")  == False
-
-def test_calculator_negative():
-    result = cl.Calculator("-13.3+(-31.2)*0.1")
-    assert result.answer == str(-13.3 + (-31.2) * 0.1)
+def test_tokenizer():
+    result = cl.Tokenizer("2+3")
+    result.run()
+    assert result.data_tokenizer == ["2","+","3"]
